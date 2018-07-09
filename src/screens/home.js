08fe3a70d1741d371a53d2449ff7cfe548e9c5bb
thomas ridenour <thomas.ridenour@gmail.com>
2018-07-09 23:50:48 +0800

    !

--- src/screens/home.js
+++ src/screens/home.js
@@ -40,7 +40,7 @@ export default class App extends Component {
                   buttonPositive: 'OK'
               };
 
-              if (grantedPreviously) {
+              if (!grantedPreviously) {
                   PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, rationale)
                       .then(granted => {
                           if (granted === PermissionsAndroid.RESULTS.GRANTED) {
